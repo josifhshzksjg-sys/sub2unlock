5eb05052f0db0e1201dc6b3fec1530e5897730ec
josifhshzksjg-sys <josifhshzksjg@gmail.com>
2026-08-05 00:49:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+
+const generateBtn = document.getElementById("generateBtn");
+
+generateBtn.addEventListener("click", () => {
+
+const channel = document.getElementById("channel").value.trim();
+const video = document.getElementById("video").value.trim();
+const telegram = document.getElementById("telegram").value.trim();
+const shorturl = document.getElementById("shorturl").value.trim();
+const download = document.getElementById("download").value.trim();
+
+if (
+channel === "" ||
+video === "" ||
+telegram === "" ||
+shorturl === "" ||
+download === ""
+){
+alert("Please fill all fields.");
+return;
+}
+
+alert("Information Saved Successfully!");
+
+});
