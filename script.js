2c2049cdcdad879dbc064628c538b3c5a6d01fd3
josifhshzksjg-sys <josifhshzksjg@gmail.com>
2026-08-05 00:12:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+const menu = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+
+menu.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+
+const generateBtn = document.getElementById("generateBtn");
+
+generateBtn.addEventListener("click", () => {
+
+const channel = document.getElementById("channel").value;
+const video = document.getElementById("video").value;
+const telegram = document.getElementById("telegram").value;
+const shorturl = document.getElementById("shorturl").value;
+
+if(channel==="" || video==="" || telegram==="" || shorturl===""){
+    alert("Please fill all fields.");
+    return;
+}
+
+alert("Unlock Link Generated Successfully!");
+
+});
+
+});
